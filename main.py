b280671b6344a46a34a392bb70c5f94eb350825a
MichiTheGenius <litzinger04@gmail.com>
2022-02-01 14:40:56 +0100

    added speed and bouncing from the walls

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Michaels Window')
 
-x,y = 400,300
-
 rect_width = 60
+x,y = 400 - rect_width/2,300 - rect_width/2
+
 
 my_rect = Rect(x,y,rect_width, rect_width, (255,0,0))
 
@@ -21,7 +21,18 @@ while running:
             running = False
 
     screen.fill((0,0,0))
-    my_rect.on_press()
+
+    my_rect.update()
+
+    if my_rect.get_x() >= width - rect_width or my_rect.get_x() <=0:
+        temp_speed_x = my_rect.get_speed_x()
+        my_rect.set_speed_x(temp_speed_x * -1)
+    
+    if my_rect.get_y() >= height - rect_width or my_rect.get_y() <=0:
+        temp_speed_y = my_rect.get_speed_y() + 5
+        my_rect.set_speed_y(temp_speed_y * -1)
+    
+
     my_rect.draw(screen)
     pygame.display.update()
 
--- rect.py
+++ rect.py
@@ -8,22 +8,37 @@ class Rect():
         self.h = h
         self.color = color
         self.rectangle = pygame.Rect(self.x, self.y, self.w, self.h)
+        self.speed_x = 5
+        self.speed_y = 0
 
-    def move_x(self, speed):
-        self.x += speed
+    def move(self):
+        self.x += self.speed_x 
+        self.y += self.speed_y
 
-    def move_y(self, speed):
-        self.y += speed
+    def set_speed_x(self, speed_x):
+        self.speed_x = speed_x
+
+    def increase_x(self, value):
+        self.speed_x += value
+
+    def set_speed_y(self, speed_y):
+        self.speed_y = speed_y
+
+    def get_speed_x(self):
+        return self.speed_x
+
+    def get_speed_y(self):
+        return self.speed_y
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
 
     def draw(self, screen):
         pygame.draw.rect(screen, (self.color), self.rectangle)
-
-    def on_press(self):
-        if self.is_clicked():
-            print('it still works!')
-
-    def is_clicked(self):
-        mouse_pos = pygame.mouse.get_pos()
-        return self.rectangle.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]
-
-
+    
+    def update(self):
+        self.rectangle = pygame.Rect(self.x, self.y, self.w, self.h)
+        self.move()
