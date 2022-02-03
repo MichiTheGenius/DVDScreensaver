9049e7cccc5f0168dafbc5ef70fa4ec6120570a2
MichiTheGenius <litzinger04@gmail.com>
2022-02-03 18:33:35 +0100

    moved the code of main to a class

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
 import pygame
 from image import Image
+from settings import *
 
 
-clock = pygame.time.Clock()
-width, height = 800, 600
+class Main():
+    def __init__(self) -> None:
+        self.clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('DVD screensaver')
+        self.screen = pygame.display.set_mode(
+            (WIDTH, HEIGHT))
+        pygame.display.set_caption('DVD screensaver')
 
+        self.dvd_logo = Image(0, 0)
 
-dvd_logo = Image(0,0)
+    def mainloop(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+            self.screen.fill(WHITE)
 
-    screen.fill((255,255,255))
+            self.dvd_logo.update(self.screen)
 
-    dvd_logo.update(screen)
+            if self.dvd_logo.get_x() >= WIDTH - self.dvd_logo.get_image_width() or self.dvd_logo.get_x() <= 0:
+                current_velocity_x = self.dvd_logo.get_velocity_x()
+                self.dvd_logo.set_velocity_x(current_velocity_x * -1)
 
-    if dvd_logo.get_x() >= width - dvd_logo.get_image_width() or dvd_logo.get_x() <=0:
-        current_speed_x = dvd_logo.get_speed_x()
-        dvd_logo.set_speed_x(current_speed_x * -1)
-    
-    if dvd_logo.get_y() >= height - dvd_logo.get_image_height() or dvd_logo.get_y() <=0:
-        current_speed_y = dvd_logo.get_speed_y()
-        dvd_logo.set_speed_y(current_speed_y * -1)
-    
+            if self.dvd_logo.get_y() >= HEIGHT - self.dvd_logo.get_image_height() or self.dvd_logo.get_y() <= 0:
+                current_velocity_y = self.dvd_logo.get_velocity_y()
+                self.dvd_logo.set_velocity_y(current_velocity_y * -1)
 
-    pygame.display.update()
+            pygame.display.update()
 
-    clock.tick(60)
+            self.clock.tick(FPS)
 
+        pygame.quit()
+        quit()
 
-pygame.quit()
-quit()
+if __name__ == '__main__':
+    m = Main()
+    m.mainloop()
